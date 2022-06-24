64a03e2a2d90f685e3c503b90efc418c6bfd3aab
Greg Burton <gburton@combitel.com.au>
2022-06-24 15:01:03 +1000

    added dev settings

--- src/microblog.py
+++ src/microblog.py
@@ -1,13 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-app.debug = True
-
-@app.route("/")
-def hello_word():
-    return "<p>Hello, World!<p>"
-
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0")
-    
+from app import app
